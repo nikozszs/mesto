19aee9469d5d6d02e37f41f5e49687590f0de812
Nikozszs <06@yandex.ru>
2024-12-26 18:00:32 +0300

    removecard

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,8 @@
 const cardsTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
-
+const card = createCard(cardData, removeCard);
+const cardTemplate = document.querySelector('.card')
 
 // @todo: Функция создания карточки
 function createCard(cardData, deleteCallback) {
@@ -11,16 +12,21 @@ function createCard(cardData, deleteCallback) {
     cardTemplate.querySelector('.card__image').src = cardData.link;
     cardTemplate.querySelector('.card__image').alt = cardData.name;
     cardTemplate.querySelector('.card__title').textContent = cardData.name;
-    cardDeleteButton.addEventListener('click', deleteCallback);
+    cardDeleteButton.addEventListener('click', function() {
+        deleteCallback(cardTemplate);
+    });
     return cardTemplate;
 }
 
 // @todo: Функция удаления карточки
+function removeCard(card) {
+    card.remove();
+}
 
 
 // @todo: Вывести карточки на страницу
 const placesList = document.querySelector('.places__list');
 initialCards.forEach(function (cardData) {
-    const card = createCard(cardData);
+    const card = createCard(cardData, removeCard);
     placesList.appendChild(card);
 })
