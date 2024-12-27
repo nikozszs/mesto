b279ae57296db02e5af09592fc6b56299c02a258
Nikozszs <06@yandex.ru>
2024-12-27 19:00:55 +0300

    upd foreach

--- scripts/index.js
+++ scripts/index.js
@@ -2,25 +2,22 @@
 const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-const createCard = (card, deleteCard) => {
+const createCard = (card) => {
     const cardTemplate = document.querySelector('#card-template').content;
-    const cardItem = document.querySelector('.card').cloneNode(true);
-    const deleteButton = cardTemplate.querySelector('.card__delete-button');
+    const cardItem = document.createElement('div');
+    cardItem.classList.add('card');
+    const deleteButton = cardTemplate.querySelector('.card__delete-button').cloneNode(true);
+    deleteButton.addEventListener('click', () => {
+        card.remove();
+    });
     cardItem.querySelector('.card__image').src = card.link;
     cardItem.querySelector('.card__title').textContent = card.name;
-    deleteButton.addEventListener('click', () => {
-        deleteCard(card);
-    });
     return cardItem;
 }
 
 // @todo: Функция удаления карточки
-const removeCard = (card) => {
-    card.remove();
-}
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach((card) => {
-    placesList.append(card);
-})
-console.log(initialCards);
+    createCard(card); 
+  });
