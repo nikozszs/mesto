0d5a43aee96e9ee9462debd91dc6119a7d87f0af
Nikozszs <06@yandex.ru>
2025-01-05 13:06:26 +0300

    remove

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,29 @@
 // @todo: DOM узлы
 const cardTemplate = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
-const cardItem = document.createElement('div');
-cardItem.classList.add('card');
 
 // @todo: Функция создания карточки
 const createCard = (card, deleteButtonFunction) => {
     const cloneCard = cardTemplate.querySelector('.card').cloneNode(true);
     const createCardImage = cloneCard.querySelector('.card__image');
+    createCardImage.setAttribute('alt', card.description);
     const createCardTitle = cloneCard.querySelector('.card__title');
     createCardTitle.textContent = card.name;
     createCardImage.src = card.link;
     const deleteButton = cloneCard.querySelector('.card__delete-button');
     deleteButton.addEventListener ('click', () => {
-      deleteButtonFunction();
+      deleteButtonFunction(cloneCard);
   }); 
 
   return cloneCard; 
-}
+};
 
 // @todo: Вывести карточки на страницу
-initialCards.forEach((card) => {
-  const cardItem = createCard(card, () => cardItem.remove());
-    placesList.append(cardItem);  
-  }); 
+function handleCardDelete(cloneCard) {
+  cloneCard.remove();
+}
+
+initialCards.forEach(function (card) {
+  const cloneCard = createCard(card, handleCardDelete);
+  placesList.append(cloneCard);
+});
