f7340e95582516a4713d765ca5b9a618aa54e7be
Nikozszs <06@yandex.ru>
2025-01-27 23:18:35 +0300

    index js

--- src/index.js
+++ src/index.js
@@ -12,3 +12,27 @@ initialCards.forEach(function (card) {
   const cloneCard = createCard(card, handleCardDelete);
   placesList.append(cloneCard);
 });
+
+// Находим форму в DOM
+const formElement = // Воспользуйтесь методом querySelector()
+// Находим поля формы в DOM
+const nameInput = // Воспользуйтесь инструментом .querySelector()
+const jobInput = // Воспользуйтесь инструментом .querySelector()
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleFormSubmit(evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    // Так мы можем определить свою логику отправки.
+    // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', handleFormSubmit);
